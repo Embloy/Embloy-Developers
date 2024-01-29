2c4b55b8354660f864fc4a57610ae9bb46c52b85
carlobortolan <carlobortolan@gmail.com>
2024-01-29 23:18:30 +0100

    Update algolia search config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -172,10 +172,8 @@ const config = {
         // The application ID provided by Algolia
         appId: 'PZJFX96675',
         // Public API key: it is safe to commit it
-        apiKey: 'f4486888d0d6fbbbd404534af4ed6370',
+        apiKey: '51b33a6d91159b4bb9d924fc9c25d7cc',
         indexName: 'developers-embloy',
-        // Optional: see doc section below
-        contextualSearch: true,
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         externalUrlRegex: 'external\\.com|domain\\.com',
         // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
