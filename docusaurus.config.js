f0056fe1db8d80b30a15567678bb26960aa1dcbe
carlobortolan <carlobortolan@gmail.com>
2024-01-29 22:27:23 +0100

    Add algolia API key

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -170,10 +170,10 @@ const config = {
       },  
       algolia: {
         // The application ID provided by Algolia
-        appId: 'YOUR_APP_ID',
+        appId: 'PZJFX96675',
         // Public API key: it is safe to commit it
-        apiKey: 'YOUR_SEARCH_API_KEY',
-        indexName: 'YOUR_INDEX_NAME',
+        apiKey: 'f4486888d0d6fbbbd404534af4ed6370',
+        indexName: 'developers-embloy',
         // Optional: see doc section below
         contextualSearch: true,
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
